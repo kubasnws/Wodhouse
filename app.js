3fa9e70f1f1b65958b0736abfea1564f0f14ca68
Jakub <j.kolewczynski@snws.pl>
2019-10-04 15:13:17 +0200

    Banner and first section

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const topNavigation = document.querySelector('nav')
 const winHeight = window.innerHeight
 const mainBanner = document.querySelector('.mainBanner')
+const showMoreChevron = document.querySelector('#mainShowMore')
+
+// easing
+const power2 = Power2.easeOut
 
 
 
@@ -12,4 +16,12 @@ const setBannerHeight = () => {
     mainBanner.style.height = `${winHeight - topNavHeight + 6}px`
 }
 
+const showHideText = (hide, show) => {
+    const tl = new TimelineMax()
+    tl.addLabel('start')
+        .to(hide, .3, { opacity: 0, y: 40, marginTop: 0, marginBottom: 0, height: 0, ease: power2 })
+        .to(show, .3, { opacity: 1, height: 'auto', ease: power2 }, 'start')
+}
+
+showMoreChevron.addEventListener('click', (e) => showHideText(e.target, e.target.closest('.mainText').querySelector('.hidden')))
 setBannerHeight()

e75ef137864426c9164920bc5dc4e973f524e7ed
Jakub <j.kolewczynski@snws.pl>
2019-10-09 16:37:30 +0200

    Almost home page

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const setBannerHeight = () => {
     mainBanner.style.height = `${winHeight - topNavHeight + 6}px`
 }
 
-const showHideText = (hide, show) => {
+const showHideText = (hide, show) => { // Chevron need showeMore class
+    if (!hide.classList.contains('showMore')) return
     const tl = new TimelineMax()
     tl.addLabel('start')
         .to(hide, .3, { opacity: 0, y: 40, marginTop: 0, marginBottom: 0, height: 0, ease: power2 })
@@ -25,3 +26,25 @@ const showHideText = (hide, show) => {
 
 showMoreChevron.addEventListener('click', (e) => showHideText(e.target, e.target.closest('.mainText').querySelector('.hidden')))
 setBannerHeight()
+
+var swiper = new Swiper('.mainProductsSlider', {
+    navigation: {
+        nextEl: '.mainNext',
+        prevEl: '.mainPrev',
+    },
+    slidesPerView: 5,
+    breakpoints: {
+        450: {
+            slidesPerView: 1,
+        },
+        720: {
+            slidesPerView: 2,
+        },
+        930: {
+            slidesPerView: 3,
+        },
+        1200: {
+            slidesPerView: 4,
+        },
+    }
+});

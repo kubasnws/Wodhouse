6bf846729aef75668ec3409ca18749f3b83b8cc5
Jakub <j.kolewczynski@snws.pl>
2019-11-21 15:50:38 +0100

    Poprawki z 21.11.2019

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const topNavigation = document.querySelector('nav')
+const navContactBar = document.querySelector('.topMenuBar')
+const { height: contactNavHeight } = navContactBar.getBoundingClientRect()
 const winHeight = window.innerHeight
 const mainBanner = document.querySelector('.mainBanner')
 const showMoreChevron = document.querySelector('#mainShowMore')
+const burgerButton = document.querySelector('.burgerButton');
+const burgerContainer = document.querySelector('.burgerMenuContainer');
+const burgerElement = document.querySelectorAll('.burgerMenuElements li');
 
 // easing
 const power2 = Power2.easeOut
 
-
+const debounce = (func, time) => {
+    var timer;
+    return event => {
+        if (timer) clearTimeout(timer);
+        timer = setTimeout(func, time || 100, event);
+    };
+}
 
 const setBannerHeight = () => {
+    const winHeight = window.innerHeight
     const {
         height: topNavHeight
     } = topNavigation.getBoundingClientRect()
@@ -24,8 +36,48 @@ const showHideText = (hide, show) => { // Chevron need showeMore class
         .to(show, .3, { opacity: 1, height: 'auto', ease: power2 }, 'start')
 }
 
+let lastScrollTop = 0;
+const menuBarAnimate = () => {
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+    if (st >= 200) {
+        TweenMax.to(topNavigation, .35, { y: -contactNavHeight, ease: Power2.easeInOut })
+        TweenMax.to('.logo img', .35, { width: 160, ease: Power2.easeInOut })
+    } else {
+        TweenMax.to(topNavigation, .35, { y: 0, ease: Power2.easeInOut })
+        TweenMax.to('.logo img', .35, { width: 200, ease: Power2.easeInOut })
+    }
+}
+
+
 showMoreChevron.addEventListener('click', (e) => showHideText(e.target, e.target.closest('.mainText').querySelector('.hidden')))
+window.addEventListener("resize", debounce(setBannerHeight)); // banner resize
+document.addEventListener('scroll', menuBarAnimate, false)
 setBannerHeight()
+
+// BURGER BUTTON
+
+const burgerButtonToggle = e => {
+
+    if (e.target.classList.contains('active')) {
+        // close menu
+        const tl = new TimelineMax()
+        tl.to(burgerContainer, .3, { scale: 0, borderRadius: '50%', ease: Power2.easeInOut })
+            .set(burgerContainer, { display: 'none' })
+    } else {
+        // open menu
+        const tl = new TimelineMax()
+        tl.set(burgerContainer, { display: 'flex' })
+            .to(burgerContainer, .3, { scale: 1, borderRadius: 0, ease: Power2.easeInOut })
+            .staggerFrom(burgerElement, .3, { y: 100, opacity: 0, scale: .6, ease: Power2.easeInOut }, .06)
+    }
+
+    e.target.classList.toggle('active');
+
+}
+
+burgerButton.addEventListener('click', e => burgerButtonToggle(e))
+
+
 
 var swiper = new Swiper('.mainProductsSlider', {
     navigation: {
@@ -48,3 +100,5 @@ var swiper = new Swiper('.mainProductsSlider', {
         },
     }
 });
+
+
